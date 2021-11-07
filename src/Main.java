7a3a0b4b69d10d11ef1dcf39d9d729c17ed6b006
tarik idrissi <tariktsid15@gmail.com>
2021-11-07 17:49:16 +0100

    looking fine

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,7 @@ public class Main {
         teachers.add(  new Teacher(4,"moubarak ahmed", 5000));
         teachers.add(  new Teacher(2,"ali houssain", 2070));
         Teacher t1=new Teacher(123,"elkant alami", 5000);
+        teachers.add(t1);
         ArrayList<Student> students=new ArrayList<>( );
 
         students.add( new Student(1,"ali ahmed", 2, 0));
@@ -21,19 +22,15 @@ public class Main {
         students.add( new Student(3,"ali ismail", 2, 0));
         students.add( new Student(6,"tarek ahmed", 2, 0));
         Student S1= new Student(434,"reda molit", 6, 0);
-
+        students.add(S1);
        // School ajial_mosta9bal=new School(teachers, students);
       // System.out.println(ajial_mosta9bal.getStudents());
 
-        S1.payFees(10800);
-        S1.payFees(10800);
-        S1.payFees(10000);
-        System.out.println(S1);
+        S1.payFees(7000);
         t1.pay(t1.getSalary());
-        t1.pay(t1.getSalary());
-        System.out.println(t1);
-        System.out.println(t1);
         System.out.println(School.getSchoolmoney());
+        System.out.println(students);
+        System.out.println(teachers);
 
     }
 
